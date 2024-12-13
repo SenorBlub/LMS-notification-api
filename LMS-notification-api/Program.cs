a7e2263b20d111708e3c2b39a932514443b2a167
SenorBlub <thomasverhappen@Gmail.com>
2024-12-13 11:29:47 +0100

    add gitignore

--- LMS-notification-api/Program.cs
+++ LMS-notification-api/Program.cs
@@ -1,6 +1,44 @@
+using DAL.NotificationDbContext;
+using DAL.Repositories;
+using DotNetEnv;
+using Logic.IRepositories;
+using Logic.IServices;
+using Logic.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+Env.Load();
+
+builder.Services.AddCors(options =>
+{
+	options.AddDefaultPolicy(corsPolicyBuilder =>
+	{
+		corsPolicyBuilder.WithOrigins(Env.GetString("FRONT_END_URL"))
+			.AllowAnyHeader()
+			.AllowAnyMethod()
+			.AllowCredentials()
+			.SetIsOriginAllowed(_ => true);
+	});
+});
+
+var connectionString =
+	$"Server={Env.GetString("DB_HOST")};Database={Env.GetString("DB_NAME")};User Id={Env.GetString("DB_USER")};Password={Env.GetString("DB_PASSWORD")};";
+
+builder.Services.AddDbContext<NotificationDbContext>(options =>
+	options.UseMySql(
+		builder.Configuration.GetConnectionString(connectionString),
+		new MySqlServerVersion(new Version(Env.GetInt("SQL_MAJOR"), Env.GetInt("SQL_MINOR"), Env.GetInt("SQL_BUILD")))
+	)
+);
+
+//!TODO auth
+
+
+builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
+builder.Services.AddScoped<INotificationService, NotificationService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
